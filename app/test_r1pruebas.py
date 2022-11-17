3d50b975a52b998c30fbcd95ab3c58b2c13eb761
dulcesg1 <sgmd682@gmail.com>
2022-11-16 19:54:30 -0600

    commit messae

--- app/test_r1pruebas.py
+++ app/test_r1pruebas.py
@@ -39,12 +39,12 @@ def test_all_pastas():
     response = app.test_client().get('http://192.168.0.7:3200/json')
     res = json.loads(response.data.decode('utf-8')).get("Pastas")
     assert response.status_code == 200
-    assert res== INFOW
+    assert res== INFO
 def test_all_pastasw():
     response = app.test_client().get('http://192.168.0.7:3200/json')
     res = json.loads(response.data.decode('utf-8')).get("Pastas")
     assert response.status_code == 200
-    assert res== INFOW    
+    assert res== INFO    
 
 def test_SOM_pasta():
     response = app.test_client().get('http://192.168.0.7:3200/json/noodles/SOM')
